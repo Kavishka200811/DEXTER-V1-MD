f6ba0a1a8a6c80e296e2ba001913b8d79bc54046
Kavishka200811 <160319899+Kavishka200811@users.noreply.github.com>
2024-05-05 00:11:32 +0530

    Update alive.js

--- commandes/alive.js
+++ commandes/alive.js
@@ -73,8 +73,8 @@ else {
     } else {
         if(!superUser) { repondre("there is no alive for this bot") ; return};
 
-      await   repondre("⚔ 𝐊𝐀𝐕𝐈𝐘𝐀 𝐌𝐃 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓 𝗢𝗪𝗡𝗘𝗥 𝗗𝗘𝗫𝗧𝗘𝗥 ⚔");
-         repondre(" *Support ravi and chaky ! cyber dexter")
+      await   repondre("⚔ 𝐊𝐀𝐕𝐈𝐘𝐀 𝐌𝐃 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓 𝗢𝗪𝗡𝗘𝗥 Kaviya� ⚔");
+         repondre(" *Support ravi and chaky ! cyber Kaviya")
      }
  } else {
 
